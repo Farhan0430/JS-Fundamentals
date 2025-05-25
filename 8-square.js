7ec4fb38a34499aa8c2a95d013fb1ecb3e40d94c
Farhan0430 <farhan.sidik66@gmail.com>
2025-05-25 20:25:45 +0200

    Square

--- 8-square.js
+++ 8-square.js
@@ -4,7 +4,7 @@ const numberarg = Number(args[0])
 
 if (!isNaN(numberarg) && numberarg > 0) {
   for (let i = 0; i < numberarg; i++) {
-  console.log('x'.repeat(numberarg));
+  console.log('X'.repeat(numberarg));
 }
 } else if (isNaN(numberarg)) {
   console.log('Missing size');
